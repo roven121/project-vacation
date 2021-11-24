dea74c5b0e6c4fa99252738cc8e3762d4cc23bf8
Rouven <roven121@gmail.com>
2021-11-24 20:08:20 +0200

    go

--- src/Api-Calls/gateway.ts
+++ src/Api-Calls/gateway.ts
@@ -4,7 +4,7 @@ const token = getItemLocalStorage("jwt");
 
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://vacation-project-3.herokuapp.com",
   headers: { Authorization: "Bearer " + token },
 });
 
